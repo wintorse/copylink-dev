1c597cf83405f57b5f50e80c3cf1848d94d8f6ff
Toma Ishii <tumaltofff@gmail.com>
2025-04-12 23:48:27 +0900

    :recycle: refactor http fallback method of copyTextLink

--- src/scripts/copyTextLink.ts
+++ src/scripts/copyTextLink.ts
@@ -18,7 +18,8 @@ async function copyToClipboard(
   notify: (message: string) => void,
   successMessage: string,
   failureMessage: string,
-  html?: string
+  html?: string,
+  fallbackElement?: HTMLElement
 ) {
   try {
     if (navigator.clipboard) {
@@ -37,15 +38,14 @@ async function copyToClipboard(
       throw new Error("Clipboard API not supported");
     }
   } catch (err) {
-    notify(failureMessage);
-    // Fallback for Firefox and HTTP URLs
-    // HTTP URLs are not supported by the Clipboard API
-    // Firefox is said to require a user gesture to copy,
-    // but the shortcuts seem to work when run within five seconds after a click
-    // https://developer.mozilla.org/en-US/docs/Web/API/Clipboard/writeText#browser_compatibility
-    const fallbackElement = html
-      ? createFallbackHtmlElement(html)
-      : createFallbackTextElement(text);
+    // notify(failureMessage);
+
+    // Fallback for HTTP URLs
+    // Clipboard API does not support HTTP URLs.
+    if (!fallbackElement) {
+      notify(failureMessage);
+      return;
+    }
     document.body.appendChild(fallbackElement);
     const range = document.createRange();
     range.selectNode(fallbackElement);
@@ -61,18 +61,13 @@ async function copyToClipboard(
   }
 }
 
-function createFallbackTextElement(text: string): HTMLTextAreaElement {
-  const textArea = document.createElement("textarea");
-  textArea.value = text;
-  return textArea;
-}
-
-function createFallbackHtmlElement(html: string): HTMLElement {
-  const container = document.createElement("div");
-  container.innerHTML = html;
-  return container;
-}
-
+/**
+ * Copies text or a link to the clipboard based on the provided argument.
+ *
+ * @param {Command} command - The action to perform.
+ *
+ * The function also displays a toast message indicating the success or failure of the copy operation.
+ */
 export async function copyTextLink(command: Command) {
   const title = getFormattedTitle();
   const url = document.URL;
@@ -96,25 +91,40 @@ export async function copyTextLink(command: Command) {
       notify,
       t("copyTitleSuccess"),
       t("copyTitleFailure"),
-      undefined
+      html
     );
   } else if (command === VALID_COMMANDS.COPY_LINK) {
+    const fallbackElement = document.createElement("textarea");
+    fallbackElement.value = title;
     await copyToClipboard(
       title,
       notify,
       t("copyLinkSuccess"),
       t("copyLinkFailure"),
-      html
+      html,
+      fallbackElement
     );
   } else if (command === VALID_COMMANDS.COPY_LINK_FOR_SLACK) {
     const emojiName = await getEmojiName();
-    const slackHtml = `${emojiName}&nbsp;<a href="${url}">${title}</a>&nbsp;`;
+    const html = `${emojiName}&nbsp;<a href="${url}">${title}</a>&nbsp;`;
+
+    const fallbackElement = document.createElement("span");
+    const emojiText = document.createTextNode(`${emojiName} `);
+    fallbackElement.appendChild(emojiText);
+    const anchor = document.createElement("a");
+    anchor.setAttribute("href", url);
+    anchor.textContent = title;
+    fallbackElement.appendChild(anchor);
+    const trailingSpace = document.createTextNode(" ");
+    fallbackElement.appendChild(trailingSpace);
+
     await copyToClipboard(
       title,
       notify,
       t("copyLinkSuccess"),
       t("copyLinkFailure"),
-      slackHtml
+      html,
+      fallbackElement
     );
   }
 }
